7b5adf9ff757ee019595249e653b6e8e66299125
BrownySprite47 <kr0085@mail.ru>
2017-01-20 13:54:21 +0400

    Pop-up окно
    - в файле индекс html создала структуру Pop-up окна
    - в файле main.css прописала стили
    - в файле script.js создала функцию для вызова поп-ап окна через
    fancybox
    
    Дополнительно:
    прописала в main.css пропущенный стиль для изображения первого блока
    контента

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    //Попап менеджер FancyBox
+    //Документация: http://fancybox.net/howto
+    //<a class="fancybox"><img src="image.jpg" /></a>
+    //<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+    $(".fancybox").fancybox();
+
+});
